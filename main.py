3cd3762e509c829a49176ab86c702a7a2a94c8ef
Wasim00000009 <144923566+Wasim00000009@users.noreply.github.com>
2023-09-13 19:32:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#1 implement a recirsive function to calculate the factorial of a given number
 i=1
+n=5
 fact=1
-n=int(input("enter the number:"))
 while(i<=n):
   fact=fact*i
   i=i+1
+
 print(fact)
